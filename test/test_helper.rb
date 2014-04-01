5ff8352519bd6269907d5310a9700572afaa8433
andreyv <jujav4ik@gmail.com>
2014-04-01 23:20:09 +0300

    make api key as optional

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,5 +16,9 @@ WHMCS.configure do |config|
   config.api_username = ENV['WHMCS_USER'] or raise "You must set the env variable WHMCS_USER"
   config.api_password = ENV['WHMCS_PASS'] or raise "You must set the env variable WHMCS_PASS"
   config.api_url = ENV['WHMCS_URL']  or raise "You must set the env variable WHMCS_URL"
-  config.api_key = ENV['WHMCS_KEY']  or raise "You must set the env variable WHMCS_KEY"
+	
+	if(ENV['WHMCS_KEY'])	
+		config.api_key = ENV['WHMCS_KEY']  
+	end
+
 end
