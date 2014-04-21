4cef1cc57ec42b57189f11e94498b8e323b896bf
andreyv <jujav4ik@gmail.com>
2014-04-21 10:09:14 +0300

    added more API tests for client requests

--- test/whmcs_client_test.rb
+++ test/whmcs_client_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WhmcsClientTest < Test::Unit::TestCase
 
-  def test__add_client_delete_client
+  def test__crud_operations_on_client
     test_data = {
       :firstname  => 'Test',
       :lastname   => 'Testson',
@@ -17,11 +17,20 @@ class WhmcsClientTest < Test::Unit::TestCase
     }
 
     saved = WHMCS::Client.add_client(test_data)
-   
+     
     if( saved['clientid'] )
+      added_note = WHMCS::Client.add_client_note(:clientid => saved['clientid'], :notes => "TestNote")
+      updated_client = WHMCS::Client.update_client(:clientid => saved['clientid'], :firstname => 'Test13')
+      
+
+      closed_client = WHMCS::Client.close_client(:clientid => saved['clientid'])
+      # finally done testing with the client
       deleted = WHMCS::Client.delete_client(:clientid => saved['clientid'])
     end
 
+    assert_equal('success', updated_client['result'])
+    assert_equal('success', closed_client['result'])
+    assert_equal('success', added_note['result'])
     assert_equal('success', saved['result'])
     assert_not_equal('', saved['clientid'])
     assert_equal('success', deleted['result'])
